d8d3ce9905b5a9b04761ca9af8fe52a959df1a19
Алина Шарапова <alinasharapova31@mail.ru>
2022-12-05 11:39:14 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector('nav');
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 930) {
+    nav.classList.add('active_nav');
+  } else{
+    nav.classList.remove('active_nav');
+  }
+})
